1a7e4aca1ef4166abffa34bf428b411574590010
Gavin Palmer <gavin@herolfg.com>
2025-03-23 11:37:58 -0500

    Update Cypress config with correct paths

--- task-manager/tests/cypress/cypress.config.js
+++ task-manager/tests/cypress/cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:19006',
+    supportFile: 'tests/cypress/support/e2e.js',
+    specPattern: 'tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    fixturesFolder: 'tests/cypress/fixtures',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
